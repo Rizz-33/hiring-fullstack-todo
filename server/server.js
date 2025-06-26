4f61482e1fb511a727d4c79cdd8973b2ffa87c88
Rizz-33 <risiniamarathunga@gmail.com>
2025-06-26 22:43:31 +0530

    feat: database connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./configs/db.js";
+
+dotenv.config();
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("[full-stack todo] server is ready!");
+});
+
 app.listen(5000, () => {
+  connectDB();
   console.log("server is running at http://localhost:5000");
 });
