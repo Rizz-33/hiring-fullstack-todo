4ff4bbf8b2278171d79e139924c880702385fb26
Rizz-33 <risiniamarathunga@gmail.com>
2025-06-26 22:51:25 +0530

    feat: todo model

---
+++ server/models/todo.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    done: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+
+export default Todo;
