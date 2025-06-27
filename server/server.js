e61237617b893999f41ba8dbe00b5571cfa205c5
Rizz-33 <risiniamarathunga@gmail.com>
2025-06-27 12:45:08 +0530

    fix: cors

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./configs/db.js";
@@ -6,6 +7,14 @@ import todoRoutes from "./routes/todo.route.js";
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(express.json());
 
